499d3f9c732939c0b94884e5f12a4755240639a6
Michael Clark <michaelclarknashville@protonmail.com>
2020-05-28 16:42:27 -0500

    order product conditional for if cart or no cart

--- ecommerceapi/views/orderproduct.py
+++ ecommerceapi/views/orderproduct.py
@@ -3,7 +3,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
-from ..models import Product, Customer,Order, PaymentType, OrderProduct
+from ..models import Product, Customer, Order, PaymentType, OrderProduct
 
 
 class OrderProductSerializer(serializers.HyperlinkedModelSerializer):
@@ -18,14 +18,35 @@ class OrderProductSerializer(serializers.HyperlinkedModelSerializer):
 class OrderProducts(ViewSet):   
 
     def create(self, request):
-       
-        new_order_product = OrderProduct()
-        order = Order.objects.get()
 
-        new_order_product.save()
+        customer = Customer.objects.filter(user_id = request.auth.user.id)
+        order = Order.object.filter(customer_id=customer.id, payment_type=None)
+
+        if order is None:
+
+            new_order = Order()
+            new_order.customer_id = customer.id
+            new_order.save()
+
+            new_order_product = OrderProduct()
+            new_order_product.customer_id = customer.id
+            new_order_product.order_id = new_order.id
+
+            new_order_product.save()
+
+            # serialize = OrderProductSerializer(new_order_product, context={'request': request}) 
+        
+        else:
+
+            new_order_product = OrderProduct()
+            new_order_product.customer_id = customer.id
+            new_order_product.order_id = order.id
+
+            new_order_product.save()
 
         serialize = OrderProductSerializer(new_order_product, context={'request': request}) 
-        return Response(serialize.data)     
+        return Response(serialize.data) 
+                
 
     def retrieve(self, request, pk=None):
         try:
